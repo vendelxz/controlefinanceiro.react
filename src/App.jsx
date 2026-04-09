f050180febb171088354b4f582b0a3303a62ebb3
vendelxz <jwend3l@gmail.com>
2026-04-08 21:12:02 -0300

    build: Estrutura inicial do Projeto em ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Este é o começo da página.</h1>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Repositórios do projeto</h2>
+          <p>Clique nos links abaixo para explorar os repositórios:</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vendelxz/finance.control/" target="_blank">
+                Frontend - Vanilla JS
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/vendelxz/api-controle-financeiro" target="_blank">
+                Backend - Java + Spring Boot
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Se conecte por aqui</h2>
+          <ul>
+            <li>
+              <a href="https://github.com/vendelxz" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+            </li>
+            <li>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
