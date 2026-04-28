d39bde7c92303853d3d4697076324f3abc867221
vendelxz <jwend3l@gmail.com>
2026-04-27 23:39:54 -0300

    feat: página de relatórios implementada (Alterações visuais pendentes)

--- src/pages/relatorios/relatorios.jsx
+++ src/pages/relatorios/relatorios.jsx
@@ -1,24 +1,51 @@
 import { CardRelatorio } from "../../components/ui/CardRelatorio";
+import { SeletorPeriodo } from "../../components/ui/SeletorPeriodo";
+import { Modal } from "../../components/ui/Modal";
 import { getRelatorioDownload, enviarRelatorioEmail } from "../../service/relatorioService";
 import { useTransacoes } from "../../hooks/useTransacoes";
 import { useState } from "react";
+import './relatorios.css';
 
 function Relatorios() {
 
-    const {transaoes, loading, erros} = useTransacoes(mes, ano, refetchTrigger);
     const [mes, setMes] = useState();
     const [ano, setAno] = useState();
-    const [refetchTrigger, setRefetchTrigger] = useState(0);
+    const [erro, setErro] = useState({});
+    const [sucesso, setSucesso] = useState(false);
 
-    const refetch = setRefetchTrigger(prev => prev +1);
-
+    //Chamar o hook de transações aqui, faz com ele mande um fetch toda hora.
+    //Quando o método é chamado na API ele já checa as transações para fazer o relatório de acordo com o mês selecionado
+    //Aumentar o número de fetchs aqui é excesso de chamdadas desnecessárias... a API vai lidar com os dados com o mês e ano de parâmetros...
 
     const handleDowload = async (event) => {
         event.preventDefault();
 
         try {
 
+            const blob = await getRelatorioDownload(mes, ano);
+            
+            setSucesso(true);
+
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `Relatorio_${mes}_${ano}.pdf`;
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+
+
+
         } catch (erro) {
+            const status = erro.response?.status;
+            console.log(erro);
+            if (status === 404) {
+                setErro({ dados: "Não há transações registradas para o período informado." });
+            }
+            else {
+                setErro({ geral: "Ocorreu um erro ao gerar o relatório. Tente novamente mais tarde." });
+            }
 
         }
     }
@@ -28,13 +55,77 @@ function Relatorios() {
 
         try {
 
-        } catch (erro) {
+            await enviarRelatorioEmail(mes, ano);
+            setSucesso(true);
 
+        } catch (erro) {
+            const status = erro.response?.status;
+            
+            if(status === 404){
+                setErro({dados: "Não há transações registradas para o período informado."});
+            }
+            else{
+                setErro({geral: "Ocorreu um erro ao gerar o relatório. Tente novamente mais tarde."});
+            }
         }
     }
 
 
-    return (<></>)
+    return (<>
+        <Modal isOpen={sucesso}
+            onClose={() => setSucesso(false)}
+            titulo="Relatório gerado com sucesso!"
+            children="Seu relatório foi gerado com sucesso. Agora ele será baixado ou enviado para seu e-mail."
+        ></Modal>
+
+        <Modal
+            isOpen={erro.geral}
+            onClose={() => setErro({})}
+            titulo="Erro ao gerar relatório"
+            children={erro.geral}
+        ></Modal>
+
+        <Modal
+            isOpen={erro.dados}
+            onClose={() => setErro({})}
+            titulo="Erro ao gerar relatório"
+            children={erro.dados}
+        ></Modal>
+
+
+        <div className="relatorios-container">
+            <h1>Relatórios</h1>
+
+            <p>Selecione o período para gerar o relatório:</p>
+
+            <SeletorPeriodo
+                mes={mes}
+                onMesChange={setMes}
+                ano={ano}
+                onAnoChange={setAno}
+            />
+            <div className="relatorios-cards">
+                <CardRelatorio
+                    icone=""
+                    titulo="Download PDF"
+                    descricao="Faça um download em PDF do seu relatório direto no site."
+                    textoBotao="Baixar relatório"
+                    onClick={handleDowload}
+                >
+                </CardRelatorio>
+
+                <CardRelatorio
+                    icone=""
+                    titulo="Enviar por e-mail"
+                    descricao="Envie o seu relatório financeiro mensal para seu e-mail cadastrado."
+                    textoBotao="Enviar por email"
+                    onClick={handleEnviarEmail}
+                >
+                </CardRelatorio>
+            </div>
+        </div>
+
+    </>)
 
 
 }
--- src/service/relatorioService.js
+++ src/service/relatorioService.js
@@ -2,12 +2,12 @@ import api from './api';
 
 //Download direto na página...
 export const getRelatorioDownload = async (mes, ano) => {
-    const resposta = await api.get('/relatorios/pfd', {params: {mes, ano}}); //A API Irá mandar um array de Bites...
+    const resposta = await api.get('/relatorios/pdf', {params: {mes, ano}, responseType: 'blob'}); //A API Irá mandar um array de Bites...
     return resposta.data;
 }
 
 //Envio de e-mail para o usuário da sessão.
 export const enviarRelatorioEmail = async (mes, ano) => {
-    const resposta =  await api.post('/relatorios/email', {params: {mes, ano}})
+    const resposta =  await api.post('/relatorios/email',null, {params: {mes, ano}})
     return resposta.data;
 }
