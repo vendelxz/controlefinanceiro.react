4d1ab9d337b2fe31a3e4812b6253e4d10a09f999
vendelxz <jwend3l@gmail.com>
2026-05-19 16:04:59 -0300

    feat: graphics initial page added to main.jsx and to /pages location

---
+++ src/pages/graphics/graphics.jsx
@@ -0,0 +1,12 @@
+
+//Página dos gráficos
+
+function Graphics() {
+    return(
+        <div>
+            <h1>Graphics</h1>
+            <p>This is the Graphics page.</p>
+        </div>
+    );
+}
+export default Graphics;
