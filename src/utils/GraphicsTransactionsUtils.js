4bdd9a21aadd1bfe1b18fa3c557b4aa855744881
vendelxz <jwend3l@gmail.com>
2026-05-20 16:17:16 -0300

    feat: GraphicsTransactionsUtils for utilities methods about transactions categories and payment types

--- src/utils/GraphicsTransactionsUtils.js
+++ src/utils/GraphicsTransactionsUtils.js
@@ -1,1 +1,29 @@
-export class GraphicsTransactionsUtils {}
+export class GraphicsTransactionsUtils {
+  static TIPOS = ['RECEITA', 'DESPESA'];
+
+  static CATEGORIAS = ['ALIMENTACAO', 'LAZER', 'CONTAS', 'SALARIO', 'OUTROS'];
+
+  static PAGAMENTOS = ['PIX', 'DINHEIRO', 'CARTAO_CREDITO', 'CARTAO_DEBITO', 'CHEQUE'];
+
+  static obterOpcoesTipos() {
+    return ['GERAL', ...this.TIPOS];
+  }
+
+  static obterOpcoesCategorias() {
+    return ['GERAL', ...this.CATEGORIAS];
+  }
+
+  static obterOpcoesPagamentos() {
+    return ['GERAL', ...this.PAGAMENTOS];
+  }
+
+  static formatarTexto(valor) {
+    if (!valor) return '';
+    if (valor === 'GERAL') return 'Todos';
+    
+    return valor
+      .replace(/_/g, ' ')
+      .toLowerCase()
+      .replace(/\b\w/g, l => l.toUpperCase());
+  }
+}
